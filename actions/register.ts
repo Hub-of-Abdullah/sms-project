6661e023ef3fbede58477e5ca1aba399bba204d1
Chanu Abdullah <abdullah.bd.cse@gmail.com>
2024-05-23 02:09:48 +0600

    completed registrstion

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,8 @@
 "use server"
 import * as z from "zod";
+import bcrypt from "bcryptjs";
 import { RegisterSchema } from "@/schemas";
+import { db } from "@/lib/db";
 
 export const register = async (values:z.infer<typeof RegisterSchema>) =>{
     const validatedFields = RegisterSchema.safeParse(values);
@@ -8,5 +10,27 @@ export const register = async (values:z.infer<typeof RegisterSchema>) =>{
     if(!validatedFields.success){
         return {error :"Invalid fields!"};
     }
+
+    const {name, email, password} = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const exitingUser = await db.user.findUnique({
+        where :{
+            email,
+        }
+    });
+
+    if(exitingUser){
+        return {error :"Email already in use !"};
+    }
+
+    await db.user.create({
+        data: {
+          name,
+          email,
+          password: hashedPassword,
+        },
+      });
+
     return {success: "Register Success..!"}
 }
