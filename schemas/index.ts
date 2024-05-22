b801c62bf0d2841914ae84a1071e1b1f5a1c3dd6
Chanu Abdullah <abdullah.bd.cse@gmail.com>
2024-05-22 20:08:15 +0600

    Created register form

--- schemas/index.ts
+++ schemas/index.ts
@@ -8,3 +8,48 @@ export const LoginSchema = z.object({
         message:"Password is required"
     })
 });
+
+
+export const RegisterSchema = z.object({
+    email : z.string().email({
+        message:"Email is required"
+    }),
+    password : z.string().min(6,{
+        message:"Minimum 6 characters requred."
+    }),
+    name : z.string().min(1,{
+        message:"Name is requred."
+    })
+});
+
+
+
+
+// import * as z from "zod";
+
+// const emailPattern = /^[a-zA-Z0-9._%+-]+@bpl.net$/;
+
+// export const LoginSchema = z.object({
+//     email: z.string().email({
+//         message: "Invalid email address"
+//     }).regex(emailPattern, {
+//         message: "Email must end with @bpl.net"
+//     }),
+//     password: z.string().min(1, {
+//         message: "Password is required"
+//     })
+// });
+
+// export const RegisterSchema = z.object({
+//     email: z.string().email({
+//         message: "Invalid email address"
+//     }).regex(emailPattern, {
+//         message: "Email must end with @bpl.net"
+//     }),
+//     password: z.string().min(6, {
+//         message: "Minimum 6 characters required."
+//     }),
+//     name: z.string().min(1, {
+//         message: "Name is required."
+//     })
+// });
