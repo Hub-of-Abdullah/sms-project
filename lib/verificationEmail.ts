8a3911f372c1ba9949f677f391ceedb2ad304305
Chanu Abdullah <mca@bpl.net>
2024-07-07 18:00:25 +0600

    updated the reset password link

--- lib/verificationEmail.ts
+++ lib/verificationEmail.ts
@@ -11,3 +11,16 @@ export const sendVerificationEmail = async (
     const mailTemplate: string = `<p>Click <a href="${confirmLink}">here</a> to confirm your email.</p>`;
     await sendMail(from, to, subject, mailTemplate);
 }
+
+
+export const sendPasswordResetEmail = async (
+    email: string,
+    token: string
+) => {
+    const resetLink = `http://localhost:3000/auth/new-password?token=${token}`;
+    const from: string = process.env.EMAIL_USER as string;
+    const to: string = email;
+    const subject: string = 'Reset your password';
+    const mailTemplate: string = `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`;
+    await sendMail(from, to, subject, mailTemplate);
+}
--- schemas/index.ts
+++ schemas/index.ts
@@ -1,24 +1,31 @@
 import * as z from "zod";
 
-export const LoginSchema = z.object({
-    email : z.string().email({
-        message:"Email is required"
+
+export const ResetSchema = z.object({
+    email: z.string().email({
+        message: "Email is required"
     }),
-    password : z.string().min(1,{
-        message:"Password is required"
+});
+
+export const LoginSchema = z.object({
+    email: z.string().email({
+        message: "Email is required"
+    }),
+    password: z.string().min(1, {
+        message: "Password is required"
     })
 });
 
 
 export const RegisterSchema = z.object({
-    email : z.string().email({
-        message:"Email is required"
+    email: z.string().email({
+        message: "Email is required"
     }),
-    password : z.string().min(6,{
-        message:"Minimum 6 characters requred."
+    password: z.string().min(6, {
+        message: "Minimum 6 characters requred."
     }),
-    name : z.string().min(1,{
-        message:"Name is requred."
+    name: z.string().min(1, {
+        message: "Name is requred."
     })
 });
 
