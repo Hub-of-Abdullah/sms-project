e19f340431beb30b107f9c4317c07406ff15370e
Chanu Abdullah <mca@bpl.net>
2024-07-10 17:57:48 +0600

    Updated

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,20 +1,49 @@
 'use client';
+// Dashboard.js
+
+// Dashboard.tsx
+
 import React from 'react';
-
+import { signOut } from "next-auth/react";
 import { useCurrentUser } from "@/hooks/use-current-user";
-function Dashboard() {
-    const user = useCurrentUser();
-    return (
-        <>
-            <div>
-                <div>Admin Dashboard page</div>
-                <div>User Email: {user?.email || 'Email not available'}</div>
-                <div>User Role: {user?.role || 'Role not available'}</div>
-                <div>User Id: {user?.id || 'Id not available'}</div>
-            </div>
+import { User } from '@prisma/client';
 
-        </>
+function Dashboard() {
+    const currentUser = useCurrentUser();
+    const user = currentUser as User; // Assuming User is your type or interface for user data
+
+    const onClickSignOut = async () => {
+        await signOut();
+        // Optionally: Reset or clear user data in local state or context if needed
+    };
+
+    return (
+        <div>
+            <div>Admin Dashboard page</div>
+            <div>User Email: {user?.email ?? 'Email not available'}</div>
+            <div>User Role: {user?.role ?? 'Role not available'}</div>
+            <div>User Id: {user?.id ?? 'Id not available'}</div>
+            <button onClick={onClickSignOut}>Sign Out</button>
+        </div>
     );
 }
 
 export default Dashboard;
+
+
+
+
+// import { auth } from "@/auth";
+
+// const TestPage = async () => {
+
+//     const session = await auth();
+//     return (
+//         <div className="">
+//             {JSON.stringify(session)}
+//         </div>
+//     );
+
+// }
+
+// export default TestPage;
